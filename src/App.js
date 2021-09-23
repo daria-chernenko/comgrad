8ae3e87876bffced9519c37f8b9e0fada5f2f778
Daria <daria.ch.developer@gmail.com>
2021-09-23 01:38:58 +0000

    At least some structure to the app

--- src/App.js
+++ src/App.js
@@ -7,8 +7,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Issue01 />
-        <Issue02 />
+        <h1>Gradient company</h1>
+        <h2>Released issues</h2>
+          <Issue01 />
+        <h2>Drafts</h2>
+          <Issue02 />
+        <footer>
+          <p>Hosted on <a href="https:
+          <p>Published on <a href="http:
+        </footer>
       </div>
     );
   }
