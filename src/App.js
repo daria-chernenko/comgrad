d6f127ec42417510f96f1ec8b966a4e0c19c02ea
Daria <daria.ch.developer@gmail.com>
2021-11-07 00:40:33 +0000

    Favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Favicon from 'react-favicon';
 import HomepageLayout from './Layout/Homepage';
 class App extends Component {
   state = { comicSize: 3 }
@@ -9,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Favicon url="/favicon.png" />
         <HomepageLayout>
         </HomepageLayout>
       </div>
