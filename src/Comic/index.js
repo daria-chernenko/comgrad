2f735ea36d69a933b311631e8900d15f9562e0a9
Daria <daria.ch.developer@gmail.com>
2021-11-30 16:03:01 +0000

    Multi download is working!

--- src/Comic/index.js
+++ src/Comic/index.js
@@ -15,22 +15,17 @@ class Comic extends Component {
     this.setState({
       wrapperClass: 'comic zoom'
     });
-    const node = this.props.children[0];
-    console.log(this.refs);
-    let domNode = ReactDOM.findDOMNode(this.refs[0]);
-    console.log(domNode);
     const style = { 
       style: {
       } 
     };
-    console.log(node);
-    domtoimage.toJpeg(domNode, style).then((dataUrl) => {
-      var link = document.createElement('a');
-      link.download = 'my-image-name.jpeg';
-      link.href = dataUrl;
-      link.click();
-      this.setState({
-        wrapperClass: 'comic'
+    Object.keys(this.refs).forEach(key => {
+      let panel = ReactDOM.findDOMNode(this.refs[key]);
+      domtoimage.toJpeg(panel, style).then((dataUrl) => {
+        var link = document.createElement('a');
+        link.download = 'my-image-name.jpeg';
+        link.href = dataUrl;
+        link.click();
       });
     });
   }
@@ -45,7 +40,6 @@ class Comic extends Component {
             {React.Children.map(this.props.children, (element, idx) => {
               return React.cloneElement(element, { ref: idx });
             })}
-            {}
           </div>
           <div className="signature">
             <a>Gradient company</a> by <a href="https:
