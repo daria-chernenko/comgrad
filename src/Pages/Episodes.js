f20c0828f8b5af853c5ddd5988f16e6b81fffc85
Daria <daria.ch.developer@gmail.com>
2022-02-04 22:39:49 +0000

    #3 episodes page formatting

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -6,20 +6,28 @@ class Episodes extends Component {
     return (
       <Container>
         <h1>Gradient company</h1>
-          <Grid container>
-           <Grid.Row textAlign='center'>
-              <Grid.Column>
-                <h2>Released issues</h2>
-                {released.map(function (Issue, index) {
-                  return <Issue key={index} />
-                })}
-                <h2>Drafts</h2>
-                {drafts.map(function (Issue, index) {
-                  return <Issue key={index} />
-                })}
+        <Grid container columns='1'>
+          <Grid.Column textAlign='center'>
+            <h2>Released issues</h2>
+          </Grid.Column>
+          {released.map(function (Issue, index) {
+            return (
+              <Grid.Column textAlign='center'>
+                <Issue key={index} />
               </Grid.Column>
-            </Grid.Row>
-          </Grid>
+            )
+          })}
+          <Grid.Column>
+            <h2>Drafts</h2>
+          </Grid.Column>
+          {drafts.map(function (Issue, index) {
+            return (
+              <Grid.Column textAlign='center'>
+                <Issue key={index} />
+              </Grid.Column>
+            )
+          })}
+        </Grid>
       </Container>
     )
   }
