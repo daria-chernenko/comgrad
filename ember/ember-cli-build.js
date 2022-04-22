78557810a370fa560caed89237e85913c365aa3d
Daria <daria.ch.developer@gmail.com>
2022-04-22 20:44:55 +0000

    fix: remove embroider

--- ember/ember-cli-build.js
+++ ember/ember-cli-build.js
@@ -3,12 +3,5 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
   });
-  const { Webpack } = require('@embroider/webpack');
-  return require('@embroider/compat').compatBuild(app, Webpack, {
-    skipBabel: [
-      {
-        package: 'qunit',
-      },
-    ],
-  });
+  return app.toTree();
 };
