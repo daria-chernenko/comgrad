c678b2982d853cec644beb9f7c2bff0aebc8fd5c
Daria <daria.ch.developer@gmail.com>
2022-06-02 21:24:57 +0000

    fix: nice beak

--- ember/config/tailwindcss-config.js
+++ ember/config/tailwindcss-config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  mode: 'jit',
   purge: ['./app*.html', './app*.hbs'],
   darkMode: false, 
   theme: {
     fontFamily: {
       vt323: ['VT323'],
       delius: ['Delius'],
+    },
+    triangles: {
+      'right-up': {
+        direction: 'right-up', 
+        size: '2em', 
+        height: '0.5em', 
+        color: 'currentColor', 
+      },
     },
     extend: {},
   },
