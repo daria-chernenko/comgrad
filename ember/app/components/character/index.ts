8148d8e51d69ce0559187cef6a76d35079dab7ca
Daria <daria.ch.developer@gmail.com>
2022-06-09 06:35:25 +0000

    feat(#47): double bubble

--- ember/app/components/character/index.ts
+++ ember/app/components/character/index.ts
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 interface CharacterArgs {
+  bubbleSizing: string;
   type: 'computer-man' | 'computer-woman' | 'zombie-man';
 }
 export default class Character extends Component<CharacterArgs> {
@@ -10,5 +11,11 @@ export default class Character extends Component<CharacterArgs> {
       'zombie-man': '🧟‍',
     };
     return types[this.args.type];
+  }
+  get sayClasses() {
+    if (this.args.bubbleSizing === 'double') {
+      return 'col-span-2';
+    }
+    return '';
   }
 }
