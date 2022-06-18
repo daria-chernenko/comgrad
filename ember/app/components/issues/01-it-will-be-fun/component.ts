470b184b79be7cb7b7f83a2189c10837306d1e20
Daria <daria.ch.developer@gmail.com>
2022-06-18 20:21:32 +0000

    feat(#51): just copy issues

--- ember/app/components/issues/01-it-will-be-fun/component.ts
+++
@@ -1,3 +0,0 @@
-import Component from '@glimmer/component';
-interface Issues01ItWillBeFunArgs {}
-export default class Issues01ItWillBeFun extends Component<Issues01ItWillBeFunArgs> {}
--- ember/app/components/issues/03-effectivity/component.ts
+++
@@ -1,3 +0,0 @@
-import Component from '@glimmer/component';
-interface Issues03EffectivityArgs {}
-export default class Issues03Effectivity extends Component<Issues03EffectivityArgs> {}
